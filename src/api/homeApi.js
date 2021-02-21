0328d7341f4e02b814b7c2fc20f930abb718eb7e
yangsonglin <yangsonglin@sykean.com>
2021-02-21 10:20:59 +0800

    homeApi

--- src/api/homeApi.js
+++ src/api/homeApi.js
@@ -2,6 +2,6 @@ import * as baseService from '../common/service'
 
 const homeApi = {
   //查询菜单
-  getMenutTree: () => baseService.get('/menu/getMenuTree')
+  getMenutTree: () => baseService.postJSON('/menu/getMenuTree')
 }
 export default homeApi
